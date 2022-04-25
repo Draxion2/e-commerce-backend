a83a57de910925ad6fad637a5df657b6615d27c2
= <holdanmcmullen@gmail.com>
2022-04-25 00:15:02 -0600

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,15 @@
-// import sequelize from the package
-const Sequelize = require("sequelize");
-require("domain").config();
+require('dotenv').config();
 
-let sequelize;
+const Sequelize = require('sequelize');
 
-// create connection to database using username and password
-sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-        host: "localhost",
-        dialect: "mysql",
-        port: 3306
-    }
-)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
